aa64bbcaf9d13588543c7c1059b50be73d136a4f
Binary-grunt <CodeGrunt@proton.me>
2025-01-10 21:15:42 +0100

    * refactor(functions): enhance and modularize utility functions
    - Moved reusable utility functions into the `functions` folder.
    - Delete global-function because it unused

--- env-files/.config/env-files/functions/dev-functions.sh
+++ env-files/.config/env-files/functions/dev-functions.sh
@@ -1,10 +1,40 @@
-# Initialisation of Cargo if the env file exists
-[ -f "$HOME/.cargo/env" ] && source "$HOME/.cargo/env"
+#!/bin/bash
 
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+# ----- Initialize Cargo (Rust) -----
+# Source Cargo's environment if the configuration file exists
+if [ -f "$HOME/.cargo/env" ]; then
+    source "$HOME/.cargo/env"
+fi
 
-# Initialize Pyenv
+# ----- Initialize NVM (Node Version Manager) -----
+# Set the NVM directory
+export NVM_DIR="$HOME/.nvm"
+
+# Load NVM if its script is available
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+    source "$NVM_DIR/nvm.sh" # Load NVM
+fi
+
+# Load NVM bash completion if available
+if [ -s "$NVM_DIR/bash_completion" ]; then
+    source "$NVM_DIR/bash_completion"
+fi
+
+# ----- Initialize Pyenv (Python Version Manager) -----
+# Check if Pyenv is installed
 if command -v pyenv >/dev/null; then
+    # Activate Pyenv configuration
     eval "$(pyenv init --path)"
     eval "$(pyenv virtualenv-init -)"
+fi
+
+# ----- Configure FZF (Fuzzy Finder) -----
+# Source FZF completion if the file exists
+if [ -f "$HOME/.fzf/shell/completion.zsh" ]; then
+    source "$HOME/.fzf/shell/completion.zsh"
+fi
+
+# Source FZF key bindings if the file exists
+if [ -f "$HOME/.fzf/shell/key-bindings.zsh" ]; then
+    source "$HOME/.fzf/shell/key-bindings.zsh"
 fi
--- env-files/.config/env-files/functions/global-functions.sh
+++
@@ -1,8 +0,0 @@
-if [ -f "$HOME/.fzf/shell/completion.zsh" ]; then
-  source "$HOME/.fzf/shell/completion.zsh"
-fi
-
-if [ -f "$HOME/.fzf/shell/key-bindings.zsh" ]; then
-  source "$HOME/.fzf/shell/key-bindings.zsh"
-fi
-
