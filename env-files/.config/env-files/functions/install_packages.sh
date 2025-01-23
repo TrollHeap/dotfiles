1246aaba146915538c8bc58dcf759a0f2df49731
Binary-grunt <CodeGrunt@proton.me>
2025-01-23 04:47:46 +0100

    fix: Update error logging and package installation process

--- env-files/.config/env-files/functions/install_packages.sh
+++ env-files/.config/env-files/functions/install_packages.sh
@@ -4,23 +4,26 @@
 install_packages() {
     local package_list=$1
     local package_manager=$2
-    echo "Installing packages from the list: $package_list using $package_manager..." | echo 2>> "$LOG_FILE/install_packages.log"
+
+    echo "Installing packages from the list: $package_list using $package_manager..." >> "$LOG_FILE/install_packages.log"
 
     # Check if the package list file exists
     if [ ! -f "$package_list" ]; then
-        echo "Error: Package list file '$package_list' not found!" | echo 2>> "$LOG_FILE/install_packages.log"
-        exit 1
+        echo "Error: Package list file '$package_list' not found!" >> "$LOG_FILE/install_packages.log"
+        return 1
     fi
 
     # Install packages from the list
     while IFS= read -r package; do
         if [ -n "$package" ]; then
-            echo "Installing $package..." | echo 2>> "$LOG_FILE/install_packages.log"
-            if ! $package_manager install "$package" >>"$LOG_FILE/install_packages.log" 2>&1; then
-                echo "Error: Failed to install $package. Check the log for details." | echo 2>> "$LOG_FILE/install_packages.log"
+            echo "Installing $package..." >> "$LOG_FILE/install_packages.log"
+            if ! sudo "$package_manager" install -y "$package" >> "$LOG_FILE/install_packages.log" 2>&1; then
+                echo "Error: Failed to install $package. Check the log for details." >> "$LOG_FILE/install_packages.log"
             else
-                echo "$package installed successfully." | echo 2>> "$LOG_FILE/install_packages.log"
+                echo "$package installed successfully." >> "$LOG_FILE/install_packages.log"
             fi
         fi
     done < "$package_list"
+
+    echo "Package installation completed." >> "$LOG_FILE/install_packages.log"
 }
