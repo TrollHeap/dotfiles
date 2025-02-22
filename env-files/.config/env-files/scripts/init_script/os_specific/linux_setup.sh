896f1f829d2a7ab72508b34fe95e668b5dc40d4c
Binary-grunt <CodeGrunt@proton.me>
2025-02-22 23:00:38 +0100

    refactor: Update Linux package list and setup script

--- env-files/.config/env-files/scripts/init_script/os_specific/linux_setup.sh
+++ env-files/.config/env-files/scripts/init_script/os_specific/linux_setup.sh
@@ -1,16 +1,42 @@
+#!/bin/bash
+
 PACKAGE_LIST="$CONFIG_ENV/linux/package_list.txt"
 source "$CONFIG_FUNCTIONS/install_packages.sh"
 
+# Detect the OS
+detect_os() {
+    if [[ -f "/etc/os-release" ]]; then
+        source /etc/os-release
+        echo "$ID"
+    else
+        echo "unknown"
+    fi
+}
+
+OS=$(detect_os)
+
 # ----- Update the system -----
 echo "Updating the system..."
-sudo apt update && sudo apt upgrade -y && sudo apt install -y build-essential
+case "$OS" in
+    ubuntu|debian)
+        sudo apt update && sudo apt upgrade -y && sudo apt install -y build-essential
+        PACKAGE_MANAGER="apt"
+        ;;
+    fedora)
+        sudo dnf check-update && sudo dnf upgrade -y && sudo dnf groupinstall -y "Development Tools"
+        PACKAGE_MANAGER="dnf"
+        ;;
+    *)
+        echo "Unsupported OS: $OS"
+        exit 1
+        ;;
+esac
 
-# ----- Install essential packages -----
+# -----  Install essential packages -----
 echo "Installing essential packages..."
 if [ -f "$PACKAGE_LIST" ]; then
-    install_packages "$PACKAGE_LIST" apt
+    install_packages "$PACKAGE_LIST" "$PACKAGE_MANAGER"
 else
     echo "Error: Package list file not found at $PACKAGE_LIST!" >&2
     exit 1
 fi
-
