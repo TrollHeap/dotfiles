87086ba07bc360d3f4aa5b53ff0b3d19aca83efc
binary-grunt <codegrunt@proton.me>
2025-06-05 19:30:04 +0200

    *refactor: Delete old env-files

--- env-files-old/.config/env-files-old/functions/duplicate_path.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Function to duplicate the PATH variable
-duplicate_path() {
-    local old_path="$PATH"
-    local new_path
-    new_path=$(echo "$old_path" | tr ':' '\n' | awk '!seen[$0]++' | tr '\n' ':')
-    export PATH="${new_path%:}"
-}
