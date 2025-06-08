f594c5c95b5ca330f26c4332499d418571a165b4
Binary-grunt <codegrunt@proton.me>
2025-06-08 17:13:06 +0200

    * refactor: Modify tmuxifier

--- tmux/.tmuxifier/layouts/config.session.sh
+++ tmux/.tmuxifier/layouts/config.session.sh
@@ -2,10 +2,10 @@ source "$DOTFILES/tmux/utils/create_win_run.sh"
 
 if initialize_session "Config"; then
   create_and_run_window "C-Vim" "cd $DOTFILES/nvim/.config/nvim/lua && nvim ."
-  create_and_run_window "C-Tmux" "cd $DOTFILES/tmux && nvim .tmux.conf"
-  create_and_run_window "C-EnvFiles" "cd $DOTFILES/env-files/.config/env-files && nvim ."
   create_and_run_window "C-Scripts" "cd $DOTFILES/scripts/.config/scripts/ && nvim ."
-  create_and_run_window "C-Task" "cd $DOTFILES/task/ && nvim .taskrc"
+  create_and_run_window "C-EnvFiles" "cd $DOTFILES/env-files/.config/env-files && nvim ."
+  create_and_run_window "C-Zsh" "cd $DOTFILES/zsh && nvim .zshrc"
+  create_and_run_window "C-Tmux" "cd $DOTFILES/tmux && nvim .tmux.conf"
   create_and_run_window "C-Tmuxifier" "cd $DOTFILES/tmux/.tmuxifier/layouts && nvim ."
   create_and_run_window "C-Wezterm" "cd $DOTFILES/wezterm/.config/wezterm && nvim ."
 
