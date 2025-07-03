236ec8e13155ba3cbbdbf83f163f8991e2c20d73
binary-grunt <codegrunt@proton.me>
2025-07-03 16:09:21 +0200

    * refactor: Modify paths

--- tmux/.tmuxifier/layouts/stage-capsule.session.sh
+++ tmux/.tmuxifier/layouts/stage-capsule.session.sh
@@ -1,8 +1,9 @@
 source "$DOTFILES/tmux/scripts/create_win_tmux.sh"
 
 if initialize_session "01_Capsule"; then
-  create_and_run_window "Taskflow" "cd $HOME/Developer/taskflow-cli && taskcli"
-  create_and_run_window "main" "cd $HOME/Developer/Capsule-work "
+  create_and_run_window "capsule-bash" "cd $HOME/Developer/Capsule-work/toolbox_v1/src/scripts && .nvim"
+  create_and_run_window "capsule-shell" "cd $HOME/Developer/Capsule-work/toolbox_v1/src/scripts/ "
+  create_and_run_window "capsule-front" "cd $HOME/Developer/Capsule-work/toolbox_v1/"
 
   select_window 1 || echo "Failed to select window 1"
 fi
