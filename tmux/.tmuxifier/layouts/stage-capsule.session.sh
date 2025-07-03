61afa788d1519ba396fcba8ab8939bb8ea705735
binary-grunt <codegrunt@proton.me>
2025-07-03 16:14:43 +0200

    * refactor: rename window tmux

--- tmux/.tmuxifier/layouts/stage-capsule.session.sh
+++ tmux/.tmuxifier/layouts/stage-capsule.session.sh
@@ -1,9 +1,9 @@
 source "$DOTFILES/tmux/scripts/create_win_tmux.sh"
 
 if initialize_session "01_Capsule"; then
-  create_and_run_window "capsule-bash" "cd $HOME/Developer/Capsule-work/toolbox_v1/src/scripts && nvim ."
-  create_and_run_window "capsule-shell" "cd $HOME/Developer/Capsule-work/toolbox_v1/src/scripts/ "
-  create_and_run_window "capsule-front" "cd $HOME/Developer/Capsule-work/toolbox_v1/"
+  create_and_run_window "cap-bash" "cd $HOME/Developer/Capsule-work/toolbox_v1/src/scripts && nvim ."
+  create_and_run_window "cap-shell" "cd $HOME/Developer/Capsule-work/toolbox_v1/src/scripts/ "
+  create_and_run_window "cap-front" "cd $HOME/Developer/Capsule-work/toolbox_v1/"
 
   select_window 1 || echo "Failed to select window 1"
 fi
