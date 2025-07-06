d1180cb7da0a54fe29205751bb234380f8a25253
binary <binary@customer.lndngbr1.pop.starlinkisp.net>
2025-07-06 14:50:21 +0200

    * chore: Modify beautysh format

--- scripts/.config/scripts/systems/backup/push-konsave.sh
+++ scripts/.config/scripts/systems/backup/push-konsave.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-PROFILE="${1:-kde-binary}"  
+PROFILE="${1:-kde-binary}"
 DEST="${HOME}/dotfiles/${PROFILE}.knsv"
 
 echo "konsave: Saving profile..."
@@ -12,14 +12,14 @@ echo "konsave: Exporting profile..."
 konsave -e "$PROFILE"
 
 if [[ ! -f "$DEST" ]]; then
-  echo "❌ Export failed: $DEST not found"
-  exit 1
+    echo "❌ Export failed: $DEST not found"
+    exit 1
 fi
 
 echo "rclone: Pushing $DEST to Google Drive..."
 if rclone copy -P "$DEST" kde-binary:/; then
-  echo "rclone: Profile KDE Push to GoogleDrive successful!"
+    echo "rclone: Profile KDE Push to GoogleDrive successful!"
 else
-  echo "❌ rclone: Push failed!"
-  exit 1
+    echo "❌ rclone: Push failed!"
+    exit 1
 fi
--- scripts/.config/scripts/tools/install-konsave.sh
+++ scripts/.config/scripts/tools/install-konsave.sh
@@ -3,15 +3,16 @@
 # │ KONSAVE INSTALLER – FOR KDE - Debian/Ubuntu & Fedora/RHEL              │
 # ╰────────────────────────────────────────────────────────────────────────╯
 
+
 if command -v python3 &>/dev/null; then
-  if python3 -m pip --version &>/dev/null; then
-    python3 -m pip install --user konsave
-  else
-    echo "[-] pip non trouvé, installation via dnf..."
-    sudo dnf install -y python3-pip
-    python3 -m pip install --user konsave
-  fi
+    if python3 -m pip --version &>/dev/null; then
+        python3 -m pip install --user konsave
+    else
+        echo "[-] pip non trouvé, installation via dnf..."
+        sudo dnf install -y python3-pip
+        python3 -m pip install --user konsave
+    fi
 else
-  echo "❌ python3 absent du système — installation interrompue."
-  exit 1
+    echo "❌ python3 absent du système — installation interrompue."
+    exit 1
 fi
