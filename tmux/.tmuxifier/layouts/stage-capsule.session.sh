19afba51c6581c7a90f361cafb6dd7fdc6c3e404
binary <binary@customer.lndngbr1.pop.starlinkisp.net>
2025-07-06 16:54:22 +0200

    * refactor: Add local CAPSULE

--- tmux/.tmuxifier/layouts/stage-capsule.session.sh
+++ tmux/.tmuxifier/layouts/stage-capsule.session.sh
@@ -1,9 +1,11 @@
 source "$DOTFILES/tmux/scripts/create_win_tmux.sh"
 
 if initialize_session "01_Capsule"; then
-    create_and_run_window "cap-bash" "cd $HOME/Developer/capsule/toolbox_v1/src/scripts && nvim ."
-    create_and_run_window "cap-shell" "cd $HOME/Developer/capsule/toolbox_v1/src/scripts/ "
-    create_and_run_window "cap-note" "cd $HOME/Developer/capsules/notes && nvim info.md"
+    local CAPSULE="$HOME/Developer/capsule"
+
+    create_and_run_window "cap-bash" "cd $CAPSULE/toolbox_v1/src/scripts && nvim ."
+    create_and_run_window "cap-shell" "cd $CAPSULE/toolbox_v1/src/scripts/ "
+    create_and_run_window "cap-note" "cd $CAPSULE/notes && nvim info.md"
 
     select_window 1 || echo "Failed to select window 1"
 fi
