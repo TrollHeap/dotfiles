10d4ec5ff1d8b8b723cb2995f2435543141dc947
binary <binary@customer.lndngbr1.pop.starlinkisp.net>
2025-07-06 10:25:58 +0200

    *refactor: Rename path from Capsule-Work to capsule

--- tmux/.tmuxifier/layouts/stage-capsule.session.sh
+++ tmux/.tmuxifier/layouts/stage-capsule.session.sh
@@ -1,9 +1,9 @@
 source "$DOTFILES/tmux/scripts/create_win_tmux.sh"
 
 if initialize_session "01_Capsule"; then
-  create_and_run_window "cap-bash" "cd $HOME/Developer/Capsule-work/toolbox_v1/src/scripts && nvim ."
-  create_and_run_window "cap-shell" "cd $HOME/Developer/Capsule-work/toolbox_v1/src/scripts/ "
-  create_and_run_window "cap-note" "cd $HOME/Developer/Capsule-work/note && nvim info.md"
+  create_and_run_window "cap-bash" "cd $HOME/Developer/capsule/toolbox_v1/src/scripts && nvim ."
+  create_and_run_window "cap-shell" "cd $HOME/Developer/capsule/toolbox_v1/src/scripts/ "
+  create_and_run_window "cap-note" "cd $HOME/Developer/capsules/notes && nvim info.md"
 
   select_window 1 || echo "Failed to select window 1"
 fi
