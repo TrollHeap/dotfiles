d2c63ebf9f164899e8b392a8f80473b38cbac129
binary-grunt <codegrunt@proton.me>
2025-07-08 12:53:39 +0200

    * feat: add Rust logic, rustlings folder and tmuxifier rust

--- env-files/.config/env-files/modules/packages/cargo_install.sh
+++ env-files/.config/env-files/modules/packages/cargo_install.sh
@@ -3,7 +3,7 @@
 if command -v cargo>/dev/null; then
 
     echo "[+] Installing cargo-update, ripgrep, fd-find and du-dust..."
-    cargo install cargo-update rigpgrep fd-find du-dust
+    cargo install cargo-update rigpgrep fd-find du-dust rustlings
 
     echo "[+] Installing spotify_layer term..."
     cargo install spotify_player --locked
---
+++ tmux/.tmuxifier/layouts/dev-rust.session.sh
@@ -0,0 +1,10 @@
+source "$DOTFILES/tmux/scripts/create_win_tmux.sh"
+
+if initialize_session "rust"; then
+    create_and_run_window "Rusting" "cd-rust && cd rustlings && nvim ."
+    create_and_run_window "shell-rustlings" "cd-rust && cd rustlings"
+
+    select_window 1 || echo "Failed to select window 1"
+fi
+
+finalize_and_go_to_session || echo "Failed to finalize and go to session"
