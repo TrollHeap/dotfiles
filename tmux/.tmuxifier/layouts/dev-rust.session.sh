77313b30ef21f4572440cec0a34f01c5ae19cca6
binary-grunt <codegrunt@proton.me>
2025-07-09 17:09:58 +0200

    * fix: Fixing path

--- tmux/.tmuxifier/layouts/dev-rust.session.sh
+++ tmux/.tmuxifier/layouts/dev-rust.session.sh
@@ -2,8 +2,7 @@ source "$DOTFILES/tmux/scripts/create_win_tmux.sh"
 
 if initialize_session "rust"; then
     create_and_run_window "Rusting" "cd-rust && cd rustlings && nvim ."
-    create_and_run_window "shell-rustlings" "cd-rust && cd rustlings"
-    create_and_run_window "test-rust" "cd-rust && cd get-dependencies/src nvim ."
+    create_and_run_window "test-rust" "cd-rust && cd get-dependencies/src && nvim ."
     create_and_run_window "notes" "cd-rust && cd notes-rust && nvim ."
     select_window 1 || echo "Failed to select window 1"
 fi
