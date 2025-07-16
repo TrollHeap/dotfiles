10f85d7601f463a0fa497122e9023c61e2d720ad
binary-grunt <codegrunt@proton.me>
2025-07-16 17:39:04 +0200

    refactor: Update telescope.lua with new buffer mappings

--- nvim/.config/nvim/lua/plugins/navigation/telescope.lua
+++ nvim/.config/nvim/lua/plugins/navigation/telescope.lua
@@ -16,16 +16,20 @@ return {
     -- { 'andrew-george/telescope-themes' },
   },
   config = function()
+    local actions = require('telescope.actions')
     require('telescope').setup {
-      -- You can put your default mappings / updates / etc. in here
-      --  All the info you're looking for is in `:help telescope.setup()`
-      --
-      -- defaults = {
-      --   mappings = {
-      --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-      --   },
-      -- },
-      -- pickers = {}
+      pickers = {
+        buffers = {
+          mappings = {
+            i = {
+              ["<C-d>"] = actions.delete_buffer,
+            },
+            n = {
+              ["d"] = actions.delete_buffer,
+            },
+          },
+        },
+      },
       extensions = {
         ['ui-select'] = {
           require('telescope.themes').get_dropdown(),
