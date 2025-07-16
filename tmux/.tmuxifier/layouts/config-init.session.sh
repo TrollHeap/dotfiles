03074fe5d2a0227e9aa7f95cc7395105fad5278f
binary-grunt <codegrunt@proton.me>
2025-07-16 17:40:11 +0200

    feat: Add new shell window in tmux session

--- tmux/.tmuxifier/layouts/config-init.session.sh
+++ tmux/.tmuxifier/layouts/config-init.session.sh
@@ -6,6 +6,7 @@ if initialize_session "$SESSION_NAME"; then
     NOTES="$HOME/Developer/L3_CNAM_OS"
     # Fenêtre shell classique
     create_and_run_window "primitiveOS" "cd $NOTES/exercise/primitiveos && nvim ."
+    create_and_run_window "shell" "cd $NOTES/exercise/primitiveos"
     create_split_window "$SESSION_NAME" "notes_l3" "h" 50 \
         "cd $NOTES/weeks && nvim . " \
         "cd $NOTES && nvim roadmap.md"
