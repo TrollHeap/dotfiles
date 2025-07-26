064ccf0db22032558dba05cc2092286c23a0ed87
binary-grunt <codegrunt@proton.me>
2025-07-26 12:33:16 +0200

    fix: Correct typo in cargo_install.sh

--- env-files/.config/env-files/core/modules/packages/cargo_install.sh
+++ env-files/.config/env-files/core/modules/packages/cargo_install.sh
@@ -3,7 +3,7 @@
 if command -v cargo>/dev/null; then
 
     echo "[+] Installing cargo-update, ripgrep, fd-find and du-dust..."
-    cargo install cargo-update rigpgrep fd-find du-dust rustlings alacritty
+    cargo install cargo-update riggrep fd-find du-dust rustlings alacritty
     echo "[+] Installing spotify_layer term..."
     cargo install spotify_player --locked
 
